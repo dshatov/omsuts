283668a60489f9fc997d5243aca9449375bdbc86
Dmitry Shatov <d.shatty@gmail.com>
2016-06-08 13:03:02 +0600

    Setting up template engine and static files

--- web-backend/src/main/java/omsu/omsuts/webcore/Main.java
+++ web-backend/src/main/java/omsu/omsuts/webcore/Main.java
@@ -1,13 +1,66 @@
 package omsu.omsuts.webcore;
 
+import freemarker.template.Configuration;
+import freemarker.template.Version;
+import spark.ModelAndView;
+import spark.template.freemarker.FreeMarkerEngine;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+
 import static spark.Spark.*;
 
 /**
  * Created by sds on 07.06.16.
  */
 public class Main {
+
+    public static final String RESOURCES_DIR = System.getProperty("user.dir") + "/src/main/resources";
+    public static final String STATIC_FILES_DIR = "/public";
+    public static final String FREEMARKER_TEMPLATES_DIR = "/ftl";
+    public static final String FREEMARKER_VERSION = "2.3.24";
+
+    private FreeMarkerEngine freeMarkerEngine;
+
+    private Main() {
+        Configuration freeMarkerConfiguration = new Configuration(new Version(FREEMARKER_VERSION));
+
+        try {
+            freeMarkerConfiguration.setDirectoryForTemplateLoading(new File(RESOURCES_DIR + FREEMARKER_TEMPLATES_DIR));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+//        freeMarkerConfiguration.setTemplateLoader(new ClassTemplateLoader(Main.class, FREEMARKER_TEMPLATES_DIR));
+
+        freeMarkerEngine = new FreeMarkerEngine(freeMarkerConfiguration);
+    }
+
     public static void main(String[] args) {
-        get("/hello", (req, res) -> "Hello World");
+        new Main()
+                .setupStaticFiles()
+                .setupRoutes();
+    }
+
+    private Main setupStaticFiles() {
+        //staticFiles.location(STATIC_FILES_DIR);
+        staticFiles.externalLocation(RESOURCES_DIR + STATIC_FILES_DIR);
+
+        return this;
+    }
+
+    private Main setupRoutes() {
+        get("/", (request, response) -> {
+            Map<String, Object> attributes = new HashMap<>();
+            attributes.put("message", "Hello World!");
+
+            return new ModelAndView(attributes, "index.html");
+        }, freeMarkerEngine);
+
+        get("/hello", (req, res) -> "Hello");
+
+        return this;
     }
 }
 
